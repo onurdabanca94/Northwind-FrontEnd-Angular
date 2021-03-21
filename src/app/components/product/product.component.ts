ce4d83e68b6579d364b533df58ada975a9e7fe1c
Onur Dabanca <onurdbnc@gmail.com>
2021-03-21 05:03:08 +0300

    Added CartItems & Another Cart Services

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -42,6 +42,10 @@ export class ProductComponent implements OnInit {
   }
 
   addToCart(product:Product){
-    this.toastrService.success("Sepete Eklendi!",product.productName)
+    if(product.productId ===1){
+      this.toastrService.error("Hata!","Bu ürün sepete eklenemez!(Örnek hata mesajı)")  
+    }else{
+      this.toastrService.success("Sepete Eklendi!",product.productName)
+    }
   }
 }
